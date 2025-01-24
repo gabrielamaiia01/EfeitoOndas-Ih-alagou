db0c0b99614ab66448d87d0f3b47c8a339a1114e
gabrielamaiia01 <gabrielamaiia01@gmail.com>
2025-01-24 19:35:23 -0300

    versão final 1

--- Página Principal/script.js
+++ Página Principal/script.js
@@ -1,11 +1,17 @@
 window.sr = ScrollReveal ({ reset: true}); 
 
-ScrollReveal().reveal('.Texto-1',  { 
-    rotate: {
-        x: 20,
-        z: 20
-    } }
- );
-
+//Card dos Membros
 ScrollReveal().reveal('.container-members',  { delay: 500 });
 ScrollReveal().reveal('.card',  { scale: 0.85 });
+
+
+//Card Sobre Nós
+ScrollReveal().reveal('.image-container',  { scale: 0.85 });
+ScrollReveal().reveal('.text-box',  { delay: 500 });
+ScrollReveal().reveal('.container2',  { delay: 1000 });
+
+
+//Header
+ScrollReveal().reveal('.Logo', { delay: 500 });
+ScrollReveal().reveal('.Cano', { delay: 500 });
+ScrollReveal().reveal('.Texto-1',  { scale: 0.85 });
