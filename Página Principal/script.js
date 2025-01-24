71a3ff54a872c4e62a7a5f32b43c9eca5ae9a7c4
gabrielamaiia01 <gabrielamaiia01@gmail.com>
2025-01-24 17:06:55 -0300

    junção dos componentes na página principal

---
+++ Página Principal/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal ({ reset: true}); 
+
+ScrollReveal().reveal('.Texto-1',  { 
+    rotate: {
+        x: 20,
+        z: 20
+    } }
+ );
+
+ScrollReveal().reveal('.container-members',  { delay: 500 });
+ScrollReveal().reveal('.card',  { scale: 0.85 });
